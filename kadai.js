c638b78b7509edf9852d58d4bd944b48491b65ea
taiga <gaitagaita69@gmail.com>
2020-06-09 23:58:54 +0900

    指摘箇所の修正(変数・関数名の修正)

--- kadai.js
+++ kadai.js
@@ -1,25 +1,25 @@
-const tasks = ["掃除","買い物","散歩"];
+const taskList = ["掃除","買い物","散歩"];
 
-const task = () => {
+const printTask = () => {
     console.log("========================");
     console.log("現在持っているタスクの一覧");
     console.log("========================");
-    for (let i = 0; i < tasks.length; i++) {
-        console.log( i + " : " + tasks[i]);
+    for (let i = 0; i < taskList.length; i++) {
+        console.log( i + " : " + taskList[i]);
     };
 }
 
-const add = () => {
+const addTask = () => {
     const name = prompt("タスクを入力してください");
     alert("新しいタスクを追加しました。");
-    tasks.push(name);
-    task();
+    taskList.push(name);
+    printTask();
 }
 
-const edit = () => {
+const editTask = () => {
     const name = prompt("「確認、追加、削除、終了」の4つのいずれかを入力してください");
 }
 
-task();
-add();
-edit();
+printTask();
+addTask();
+editTask();
